d1f62bda36bd132a6c997a3257f796410bba3959
João Augusto <joao.augusto@codelathe.com>
2022-04-15 18:57:15 -0300

    build script

--- build.sh
+++ build.sh
@@ -3,15 +3,35 @@
 dc="docker-compose";
 dcp="docker-compose -f docker-compose-prod.yml";
 
-# echo
-# echo '### INSTALL SPA DEPENDENCIES ###'
+echo
+echo '### INSTALL SPA DEPENDENCIES ###'
 # $dc run --rm spa npm install
+cd spa/
+npm install
+cd ..
 
 echo
 echo '### BUILD THE SPA DIST VERSION ###'
-$dc run --rm spa npm cache clean --force
-$dc run --rm spa npm run build --mode=production
+# $dc run --rm spa npm run build --mode=production
+cd spa/
+npm run build --mode=production
+cd ..
 
-# echo
-# echo '### ENSURE THAT THE API BUILD FOLDER EXISTS ###'
-# $dc run --rm api mkdir -p /var/dist/build
+
+echo
+echo '### INSTALL API DEPENDENCIES ###'
+# $dc run --rm spa npm run build --mode=production
+cd api/
+npm install
+cd ..
+
+
+echo
+echo '### MOVE BUILD FOLDER ###'
+cp -rf spa/dist/* env/prod/spa/dist/build
+
+
+echo
+echo '### BUILD THE PROD ENV ###'
+$dcp build
+
